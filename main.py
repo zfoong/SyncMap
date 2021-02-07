0e6981c122698767d718c0d84bd2e3c291c64ac3
tham yik foong <tham_yikfoong@outlook.com>
2021-02-08 05:02:23 +0800

    upload main and utils file

---
+++ main.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 21:28:21 2021
+
+@author: zfoong
+"""
+
+import cv2     
+import math  
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D  
+import pandas as pd
+import numpy as np   
+from tqdm import tqdm
+import os
+import random
+from utils import MC_gen
+
+random.seed(99)
+
+def input_encoding(S_t, X_t, decay_rate):
+    return X_t * np.exp(-decay_rate) + S_t
+    
+def weight_update(x, w):
+    PS_NS = x > 0.1
+    PS_NS.astype('float64')
+    PS = np.count_nonzero(PS_NS)
+    NS = len(PS_NS) - np.count_nonzero(PS_NS)
+    if PS > 1 and NS > 1:
+        PS_NS = PS_NS.reshape((-1,1))
+        cp = np.sum(PS_NS * w) / PS
+        cn = np.sum((1 - PS_NS) * w) / NS
+        return w + a * ( PS_NS * cp + (1 - PS_NS) * cn) / np.linalg.norm(w-cp)
+    return w
+
+def mat_norm(a):
+    a = a - a.mean(axis=0)
+    a = a / np.abs(a).max(axis=0)
+    return a
+
+# Param Init
+a = 0.01
+decay_rate = 1
+
+# Variable Init
+k = 3 # Dimension variable
+N = 10 # Input size
+w = 2.0 * np.random.random((N, k)) - 1.0
+
+data = MC_gen().data_gen()
+# x = np.zeros(N)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ims = []
+
+for i, j in tqdm(enumerate(data[0])):
+    x = np.zeros(N)
+    for k, l in enumerate(j):
+        s_t = np.zeros(N)
+        s_t[l] = 1
+        x = input_encoding(s_t, x, decay_rate)
+        prev_w = w
+        w = weight_update(x, w)
+        w = mat_norm(w)
+        if not np.array_equal(prev_w, w):
+            img = ax.scatter(w[:,0], w[:,1], w[:,2], color="blue")
+            ims.append([img])
+
+print("Generating animation...")
+ani = animation.ArtistAnimation(fig, ims, interval=5, blit=True,
+                                repeat_delay=100)
+print("Generating animation completed!")
+print("Saving animation as MP4")
+ani.save('movie.mp4')
+print("Saving animation as MP4 completed!")
+# plt.show()
+
+
---
+++ utils.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  6 23:54:12 2021
+
+@author: zfoong
+"""
+
+import numpy as np
+import random
+
+random.seed(99)
+np.random.seed(99)
+
+class MC_gen():
+    def __init__(self):
+        self.N = 10 # Possible state
+        self.possible_trans_rate = 0.3 # Rate of element in TP being nonzero        
+        self.data_count = 500 # Total of sequence generated
+        self.maxlen = 20 # Maximum length of each sequence
+        self.init_state = [0, 3] # Initial state
+        self.terminal_state = [1] # End state
+        self.data = []
+    
+    def TP_matrix_gen(self, N, rate):
+        tp = np.random.binomial(1, rate, [N, N]) # Transition Probability Matrix
+        tp = tp.astype('float64')
+        for i, j in enumerate(tp):
+            ones = j.nonzero()[0]
+            rand_to_1 = np.random.dirichlet(np.ones(len(ones)),size=1)
+            for k, l in enumerate(ones):
+                tp[i][l] = rand_to_1[0][k]
+        return tp
+    
+    def seq_gen(self, init_state, terminal_state, maxlen):
+        current_state = np.random.choice(init_state, replace=True)
+        d = [current_state]
+        for _ in range(maxlen):
+            if np.sum(self.TP[current_state]) == 0 or current_state in terminal_state:
+                break
+            next_state = np.random.choice(self.N, replace=True, p=self.TP[current_state])
+            d.append(next_state)
+            current_state = next_state
+        return d
+        
+    def data_gen(self):
+        self.TP = self.TP_matrix_gen(self.N, self.possible_trans_rate)
+        self.data.append([self.seq_gen(self.init_state, 
+                                       self.terminal_state, 
+                                       self.maxlen) 
+                          for _ in range(self.data_count)])
+        return self.data
+
+
